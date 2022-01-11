c2881568aeb1c2cc7d899499525667408d9ff269
Villalpando Julien <Villalpando.julien@gmail.com>
2022-01-11 16:40:49 +0100

    08_play_with_capitalized_strings

---
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/08_play_with_capitalized_strings/src/index.js
@@ -0,0 +1,5 @@
+const question = "spartan, what is your profession?!";
+
+// a constant `capitalizedSentence` bound to a string: "Spartan, what is your profession?!"
+// using some of the previous string methods and the constant `question`
+const capitalizedSentence=question.charAt(0).toUpperCase()+question.slice(1);
