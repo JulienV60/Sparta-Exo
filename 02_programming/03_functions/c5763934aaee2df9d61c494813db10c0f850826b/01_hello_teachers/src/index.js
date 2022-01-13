24ff1b812fbc8f00c62d47a24ebc001202ce5bbe
Villalpando Julien <Villalpando.julien@gmail.com>
2022-01-13 11:15:27 +0100

    01_hello_teachers

---
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/01_hello_teachers/src/index.js
@@ -0,0 +1,15 @@
+function sayHelloToTeachers(teachers) {
+
+  console.log(`Hello ${spartaTeachers[0]}!`);
+  console.log(`Hello ${spartaTeachers[1]}!`);
+  console.log(`Hello ${spartaTeachers[2]}!`);
+  console.log(`Hello ${spartaTeachers[3]}!`);
+  console.log(`Hello ${spartaTeachers[4]}!`);
+
+  
+
+}
+
+// Do not remove last lines, it is for tests
+const spartaTeachers = ["Thomas", "Fenn", "Florian", "Martin", "Louis"];
+module.exports = sayHelloToTeachers;
