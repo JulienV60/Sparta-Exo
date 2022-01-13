f19b403269872c9f0855043eefac615d06d00a96
Villalpando Julien <Villalpando.julien@gmail.com>
2022-01-13 10:24:56 +0100

    09_exoplanets_bonus

---
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/09_exoplanets_bonus/src/index.js
@@ -0,0 +1,42 @@
+/ Don't modify these lines for tests to work properly.
+const star = require("./data/starData");
+const planet = require("./data/exoplanetData");
+
+// Your code here:
+let canHabitateLife;
+
+// Don't modifiy this line for test to work properly.
+module.exports = canHabitateLife;
+
+const b = {
+  orbitalPeriod: 1.51,
+  distanceToStar: 0.0115,
+  planetRadius: 1.12,
+  planetMass: 1.02,
+  planetDensity: 0.73, 
+  surfaceGravity: 0.81,
+};
+const c = {
+  orbitalPeriod: 2.42,
+  distanceToStar: 0.0158,
+  planetRadius: 1.10,
+  planetMass: 1.16,
+  planetDensity: 0.88,
+  surfaceGravity: 0.96,
+};
+module.exports = {
+  mass: 0.77,
+  radius: 0.91,
+  rotationStability: true,
+  habitalZone: true,
+};
+module.exports = {
+  spectralClass: "M",
+  temperature: 2550,
+  mass: 0.089,
+};
+
+//Can this planet be home for life ? 
+let planetRadius >= 1 && planetRadius <= 2.5 ? true : false;
+let planetMass >= 0.32 ? true : false; 
+
