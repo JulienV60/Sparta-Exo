d46ae2aefab53946004ba5494c757956a371143e
Villalpando Julien <Villalpando.julien@gmail.com>
2022-01-18 11:32:36 +0100

    GOOD

---
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/02_for_each_with_a_loop/src/index.js
@@ -0,0 +1,9 @@
+function forEachLoop(people) {
+  for (let i = 0;i < people.length; i++){console.log(`* ${people[i]}`);}}
+
+// Code the function here.
+
+
+
+// Do not remove the following lines, it is for tests
+module.exports = forEachLoop;
