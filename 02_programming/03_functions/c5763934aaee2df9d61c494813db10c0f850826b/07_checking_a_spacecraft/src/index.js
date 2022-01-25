be522c6b9cb9f472c3a0167c91be2e596e655154
Villalpando Julien <Villalpando.julien@gmail.com>
2022-01-25 10:49:26 +0100

    Let's sail !

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
@@ -1,5 +1,28 @@
-// Code here
+// Code her
+const stealthyFuelLevelCheck = (number) => {
+  Number.isInteger(number);
+  if (number >= 20) {
+    return "Fuel level: green";
+  }
+  if (number >= 10) {
+    return "Fuel level: yellow";
+  } else {
+    return "Fuel level: red";
+  }
+};
 
-// Do not remove last lines, it is for tests
+const stealthyCargoHoldCheck = (array) => {
+  Array.isArray(array.length);
+  console.log(array.length);
+  if (array.length == 4) {
+    return "Full";
+  }
+  if (array.length < 4) {
+    return "Spaces available: 2";
+  }
+  if (array.length > 4) {
+    return `Over capacity by ${array.length - 4} items.`;
+  }
+};
 
 module.exports = { stealthyFuelLevelCheck, stealthyCargoHoldCheck };
