f1c24ff57b96b37186e1a4ff35165aee2aa4af70
Villalpando Julien <Villalpando.julien@gmail.com>
2022-01-25 19:05:16 +0100

    If/else

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/08_consecutive_count/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/08_consecutive_count/src/index.js
@@ -1,5 +1,22 @@
-function consecutiveCount() {
-  // Code here
+function consecutiveCount(name, letter) {
+  let counterLetter = 0;
+  let streak = 0;
+
+  for (let i = 0; i < name.length; i++) {
+    if (name.charAt(i) === letter) {
+      counterLetter++;
+      if (counterLetter > streak) {
+        streak = counterLetter;
+      }
+    } else {
+      counterLetter = 0;
+    }
+  }
+  if (streak === 0) {
+    return 0;
+  } else {
+    return streak;
+  }
 }
 
 // Do not remove the following line, it is for tests
