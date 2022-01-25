8134fc19b0b56fafbae533dbd017c69d27edcdda
Villalpando Julien <Villalpando.julien@gmail.com>
2022-01-25 11:19:03 +0100

    On compte

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/07_letter_count/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/07_letter_count/src/index.js
@@ -1,6 +1,15 @@
-function letterCount() {
-  // Code here
+function letterCount(name, letter) {
+  let comptage = 0;
+
+  for (let i = 0; i < name.length; i++) {
+    if (name.charAt(i) == letter) {
+      comptage += 1;
+    }
+  }
+  return comptage;
 }
+
+// Code here
 
 // Do not remove the following line, it is for tests
 module.exports = letterCount;
