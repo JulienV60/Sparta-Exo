089a67881fc0987fb57900d9959ef3e37e05a3d6
Villalpando Julien <Villalpando.julien@gmail.com>
2022-01-31 15:40:28 +0100

    EZ

---
+++ 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/06_object_signature_key/src/index.ts
@@ -0,0 +1,11 @@
+type Supplies = {
+  [key: string]: number;
+};
+
+const supplies: Supplies = {};
+
+// ↓ Uncomment the line below if you want to test it ↓
+// supplies.monitor = 10;
+
+// Leave the line below for tests to work properly.
+export { supplies };
