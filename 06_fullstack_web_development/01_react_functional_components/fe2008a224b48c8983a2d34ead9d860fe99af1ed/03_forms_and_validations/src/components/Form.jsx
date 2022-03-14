ce476e7544eba3ff40345835866f20304caa5c4f
Villalpando Julien <villalpando.julien@gmail.com>
2022-03-14 23:13:46 +0100

    ok

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
@@ -1,5 +1,47 @@
 import React from "react";
 
-const Form = () => "Code here";
+const Form = () => {
+  const [showSpace, setShowSpace] = React.useState(false);
+  const [showSend, setShowSend] = React.useState(false);
+  function showSpaceAllowed(event) {
+    const result = event.target.value;
+    if (result !== null) {
+      if (result.includes(" ")) {
+        return setShowSpace(<p>no spaces allowed</p>);
+      } else {
+        return setShowSpace(false);
+      }
+    }
+  }
+  function onSubmit(eventt) {
+    const result = eventt.target.value;
+    if (result !== null) {
+      return setShowSend(<p>Coucou</p>);
+    } else {
+      return setShowSend(false);
+    }
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="col">
+          <label>First Name</label>
+          <input
+            onChange={showSpaceAllowed}
+            id="test"
+            type="text"
+            className="form-control"
+            placeholder="First Name"
+          ></input>
+          {showSpace}
+          <button type="submit" className="btn btn-primary" id="test2">
+            Send
+          </button>
+          {showSend}
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
