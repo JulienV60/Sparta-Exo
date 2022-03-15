9768b2d05f2c7bab228e114c70e8eb38e6f1ab28
Villalpando Julien <villalpando.julien@gmail.com>
2022-03-15 09:54:10 +0100

    bientot finis le 4

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/04_advanced_form_validation_bonus/src/components/ContactForm/index.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/04_advanced_form_validation_bonus/src/components/ContactForm/index.jsx
@@ -1,7 +1,92 @@
 import React from "react";
 
 const ContactForm = () => {
-  return "Code here!";
+  const nameRegex = /^[_A-zàâäèéëêïîöôûüù]+((-|\s)*[_A-zàâäèéëêïîöôûüù])*$/gi;
+  const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const messageRegex = /^.{20,400}$/;
+  const [showFirstName, setShowFirstName] = React.useState(false);
+  const [showLastName, setShowLastName] = React.useState(false);
+  const [showEmail, setShowEmail] = React.useState(false);
+  const [showMessage, setShowMessage] = React.useState(false);
+  function firstName() {
+    if (document.getElementById("first-name") !== null) {
+      const result = document.getElementById("first-name").value;
+      if (result.match(nameRegex) === null) {
+        return setShowFirstName(<p>Minimum 1 letter,no special characters expect `-`` or space between words</p>);
+      } else {
+        setShowFirstName(false);
+      }
+    }
+  }
+  function lastName() {
+    if (document.getElementById("last-name") !== null) {
+      const result = document.getElementById("last-name").value;
+      if (result.match(nameRegex) === null) {
+        return setShowLastName(<p>Minimum 1 letter,no special characters expect `-`` or space between words </p>);
+      } else {
+        setShowLastName(false);
+      }
+    }
+  }
+
+  function email() {
+    if (document.getElementById("email") !== null) {
+      const result = document.getElementById("email").value;
+      if (result.match(emailRegex) === null) {
+        return setShowEmail(<p>This is not a valid email adress </p>);
+      } else {
+        setShowEmail(false);
+      }
+    }
+  }
+  function message() {
+    if (document.getElementById("message") !== null) {
+      const result = document.getElementById("message").value;
+      if (result.match(messageRegex) === null) {
+        return setShowMessage(<p>Minimum 20 and maximum 400 characters </p>);
+      } else {
+        setShowMessage(false);
+      }
+    }
+  }
+  return (
+    <div>
+      <form>
+        <label>First Name</label>
+        <input
+          className=".form-control"
+          id="first-name"
+          type="text"
+          onChange={firstName}
+          placeholder="First Name"
+        ></input>
+        {showFirstName}
+
+        <label>Last Name</label>
+        <input
+          className=".form-control"
+          id="last-name"
+          ontype="text"
+          onChange={lastName}
+          placeholder="Last Name"
+        ></input>
+        {showLastName}
+
+        <label>Email </label>
+        <input className=".form-control" id="email" type="text" onChange={email} placeholder="Email"></input>
+        {showEmail}
+
+        <label>Message </label>
+        <input className=".form-control" id="message" type="text" onChange={message} placeholder="Message"></input>
+        {showMessage}
+
+        <button type="submit" id="test2">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
